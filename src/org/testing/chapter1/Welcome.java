d48813abc322126d174249ea2be0bca2a0f5841d
Vijay Kumar <rvk1327@gmail.com>
2026-02-17 19:59:49 +0530

    updated on 17Feb2026

---
+++ src/org/testing/chapter1/Welcome.java
@@ -0,0 +1,12 @@
+package org.testing.chapter1;
+
+public class Welcome {
+
+	public static void main(String[] str) {			
+		
+		System.out.println("Welcome to java");
+		
+		int first_value = 100;
+	}
+
+}
